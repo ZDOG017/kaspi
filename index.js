3fad79beb74ec1a390915c5295e803dc64d4a533
Soltan <iamkazakh02@gmail.com>
2024-07-26 01:13:59 +0500

    init

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 
-// Define the URL and headers
 const url = 'https://kaspi.kz/yml/product-view/pl/filters?text=NVIDIA+GTX+1660+SUPER&hint_chips_click=false&page=0&all=false&fl=true&ui=d&q=%3AavailableInZones%3AMagnum_ZONE1&i=-1&c=750000000';
 const headers = {
   'Accept': 'application/json, text/*',
@@ -13,14 +12,9 @@ const headers = {
   'Accept-Encoding': 'gzip, compress, deflate, br'
 };
 
-axios.get(url, { headers, maxRedirects: 5 })
-  .then(response => {
-    // Log the entire response
-    console.log('Response status:', response.status);
-    console.log('Response headers:', response.headers);
-    console.log('Response data:', response.data);
-
-    // Check the structure of the response
+const fetchData = async (retryCount = 0) => {
+  try {
+    const response = await axios.get(url, { headers, maxRedirects: 5 });
     if (response.data && response.data.data && response.data.data.cards) {
       const data = response.data.data.cards; // Extract the array of products
       data.forEach(product => {
@@ -42,21 +36,19 @@ axios.get(url, { headers, maxRedirects: 5 })
     } else {
       console.error('Unexpected response structure:', response.data);
     }
-  })
-  .catch(error => {
-    // Log the error details
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.error('Error status:', error.response.status);
-      console.error('Error headers:', error.response.headers);
-      console.error('Error data:', error.response.data);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error('Error request:', error.request);
+  } catch (error) {
+    if (error.response && error.response.status === 429) {
+      const retryAfter = parseInt(error.response.headers['retry-after'] || '1', 10);
+      console.warn(`Rate limit exceeded. Retrying after ${retryAfter} seconds...`);
+      if (retryCount < 5) {
+        setTimeout(() => fetchData(retryCount + 1), retryAfter * 1000);
+      } else {
+        console.error('Max retries reached. Please try again later.');
+      }
     } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error('Error message:', error.message);
+      console.error('Error fetching data:', error.message);
     }
-    console.error('Error config:', error.config);
-  });
+  }
+};
+
+fetchData();
