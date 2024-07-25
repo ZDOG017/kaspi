7dfe21193ac25bd654c231e4f2c16868d50d323c
Soltan <iamkazakh02@gmail.com>
2024-07-26 01:54:29 +0500

    init

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const puppeteer = require('puppeteer');
   // Launch a new browser instance
   const browser = await puppeteer.launch({
     headless: true, // Set to false if you want to see the browser window
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   // Open a new page
@@ -20,24 +19,31 @@ const puppeteer = require('puppeteer');
     waitUntil: 'networkidle2', // Wait until the network is idle
   });
 
+  // Wait for the product cards to be visible
+  await page.waitForSelector('.item-card', { timeout: 30000 });
+
   // Extract product data
   const products = await page.evaluate(() => {
     const productElements = document.querySelectorAll('.item-card');
     const productData = [];
 
     productElements.forEach(product => {
-      const id = product.getAttribute('data-id');
-      const title = product.querySelector('.item-card__name').innerText;
-      const brand = product.querySelector('.item-card__brand').innerText;
-      const price = product.querySelector('.item-card__price').innerText;
-      const rating = product.querySelector('.item-card__rating-stars').getAttribute('data-rating');
-      const reviewsQuantity = product.querySelector('.item-card__reviews').innerText;
-      const shopLink = product.querySelector('a').href;
+      const id = product.getAttribute('data-product-id');
+      const titleElement = product.querySelector('.item-card__name a');
+      const priceElement = product.querySelector('.item-card__prices .item-card__prices-price');
+      const ratingElement = product.querySelector('.item-card__rating .rating');
+      const reviewsElement = product.querySelector('.item-card__rating a');
+      const shopLinkElement = product.querySelector('.item-card__image-wrapper');
+
+      const title = titleElement ? titleElement.innerText : 'N/A';
+      const price = priceElement ? priceElement.innerText : 'N/A';
+      const rating = ratingElement ? ratingElement.className.match(/_([0-9]+)/)[1] : 'N/A';
+      const reviewsQuantity = reviewsElement ? reviewsElement.innerText.match(/\((\d+) отзыв/)[1] : 'N/A';
+      const shopLink = shopLinkElement ? shopLinkElement.href : 'N/A';
 
       productData.push({
         id,
         title,
-        brand,
         price,
         rating,
         reviewsQuantity,
@@ -52,7 +58,6 @@ const puppeteer = require('puppeteer');
   products.forEach(product => {
     console.log(`Product ID: ${product.id}`);
     console.log(`Title: ${product.title}`);
-    console.log(`Brand: ${product.brand}`);
     console.log(`Price: ${product.price}`);
     console.log(`Rating: ${product.rating}`);
     console.log(`Reviews: ${product.reviewsQuantity}`);
