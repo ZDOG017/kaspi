c16e35ad27d2c7b02c5296d9a6cd31394daf8ff1
Soltan <iamkazakh02@gmail.com>
2024-07-26 01:08:08 +0500

    init

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const headers = {
   'Referer': 'https://kaspi.kz/shop/search/?text=NVIDIA%20GTX%201660%20SUPER&hint_chips_click=false',
   'Cookie': 'ks.tg=71; k_stat=aa96833e-dac6-4558-a423-eacb2f0e53e4; kaspi.storefront.cookie.city=750000000; current-action-name=Index',
   'Connection': 'keep-alive',
+  'Accept-Encoding': 'gzip, compress, deflate, br'
 };
 
-axios.get(url, { headers })
+axios.get(url, { headers, maxRedirects: 5 })
   .then(response => {
-    // Log the entire response to see what we are getting
+    // Log the entire response
+    console.log('Response status:', response.status);
+    console.log('Response headers:', response.headers);
     console.log('Response data:', response.data);
 
     // Check the structure of the response
@@ -41,5 +44,19 @@ axios.get(url, { headers })
     }
   })
   .catch(error => {
-    console.error('Error fetching data:', error);
+    // Log the error details
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Error status:', error.response.status);
+      console.error('Error headers:', error.response.headers);
+      console.error('Error data:', error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('Error request:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error message:', error.message);
+    }
+    console.error('Error config:', error.config);
   });
